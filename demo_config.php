5926fd787729aee05e2208031b76d321e5dce2d8
Dương Văn Sỹ <91702958+dvsteam@users.noreply.github.com>
2024-02-27 02:20:28 +0700

    Add files via upload

---
+++ demo_config.php
@@ -0,0 +1,11 @@
+<?php
+const CONFIG = [    
+    'DOMAIN_WEB' => "xxxxx.net", // *Tên miền web của bạn
+
+    'DATABASE' => [ // vào file .env của src web để xem thông tin TK, MK DB
+        'HOST' => "localhost", // Mặt định là localhost (không cần sửa)
+        'DBNAME' => "bbbb" , // *DB_DATABASE trong file.env
+        'USERNAME' => "aaaa", // *DB_USERNAME trong file .env
+        'PASSWORD' => "xxxx", // *DB_PASSWORD trong file .env
+    ]
+];
---
+++ gate.php
@@ -0,0 +1,13 @@
+<?php
+if ($gate == "ViettelMoney_DVS") {
+	include('Views/ViettelMoney_DVS.php');
+}
+if ($gate == "AutoVCB_DVS") {
+	include('Views/VCB_DVS.php');
+}
+if ($gate == "AutoACB_DVS") {
+	include('Views/ACB_DVS.php');
+}
+if ($gate == "AutoCard_DVS") {
+	include('Views/data_CardDVS.php');
+}
