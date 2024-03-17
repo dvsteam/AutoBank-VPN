7fad07acc3b01b280201d459f6dd403e2174cfe4
Dương Văn Sỹ <91702958+dvsteam@users.noreply.github.com>
2024-03-17 13:37:01 +0700

    Fix update nhầm haha

--- demo_config.php
+++ demo_config.php
@@ -1,20 +1,11 @@
 <?php
-const CONFIG = [
-    'DOMAIN_WEB' => "dvsteam.net", //Tên miền web của bạn
+const CONFIG = [    
+    'DOMAIN_WEB' => "xxxxx.net", // *Tên miền web của bạn
+
     'DATABASE' => [ // vào file .env của src web để xem thông tin TK, MK DB
-        'HOST' => "localhost",
-        'DBNAME' => "bbbb" , // DB_DATABASE trong file.env
-        'USERNAME' => "aaaa", // DB_USERNAME trong file .env
-        'PASSWORD' => "xxxx", // DB_PASSWORD trong file .env
-    ],
-    'GATE' => [
-        'CARD_DVS' => [
-            'hookCard_DVS' => "xxx", //link hookcard mà dvsteam đã setup trước đó
-            'WEBHOOK' => "xxx", // link (Địa Chỉ Thông Báo) trong web admin mục (Cấu Hình Thanh Toán)
-            'KEY_CARD' => "xxxxx", // Key API https://gachthedvs.net
-            'CHIEUKHAU' => "1", // Chiếu Khấu Thẻ
-            'CHAT_ID' => "102345xxxx" // lấy ID telegram tại https://t.me/getmyid_bot
-            // Bấm Start bot https://t.me/AutoCardDVS_Bot để nhận thông báo giao dịch Card
-        ]
-    ],
+        'HOST' => "localhost", // Mặt định là localhost (không cần sửa)
+        'DBNAME' => "bbbb" , // *DB_DATABASE trong file.env
+        'USERNAME' => "aaaa", // *DB_USERNAME trong file .env
+        'PASSWORD' => "xxxx", // *DB_PASSWORD trong file .env
+    ]
 ];
--- gate.php
+++ gate.php
@@ -1,10 +1,19 @@
 <?php
 if ($gate == "ViettelMoney_DVS") {
-	include('Views/ViettelMoney_DVS.php');
+	include('Views/MBBank_DVS.php');
 }
 if ($gate == "AutoVCB_DVS") {
 	include('Views/VCB_DVS.php');
 }
 if ($gate == "AutoACB_DVS") {
 	include('Views/ACB_DVS.php');
+}
+if ($gate == "AutoCard_DVS") {
+	include('Views/data_CardDVS.php');
+}
+if ($gate == "AutoViettin_DVS") {
+	include('Views/Viettin_DVS.php');
+}
+if ($gate == "AutoMBBank_DVS") {
+	include('Views/MBBank_DVS.php');
 }
